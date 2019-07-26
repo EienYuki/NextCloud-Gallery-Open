e0c2ae4a7321daa8724bbf448f3547691d524f5f
Yuki <midorin452@gmail.com>
2019-07-26 22:08:28 +0800

    修正 出現的位置不正確

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-const div = document.createElement('div')
-div.innerHTML = 
+const desktop = 
 `
-<li data-id="galleryAPPView" class="" tabindex="-1">
-	<a id="galleryAPPViewbtn" aria-label="相簿">
-			<svg width="20" height="20" viewBox="0 0 20 20" alt="">
-				<image x="0" y="0" width="20" height="20" preserveAspectRatio="xMinYMin meet" xlink:href="/apps/gallery/img/app.svg?v=d7726c1d" class="app-icon"></image>
-			</svg>
-		<span>相簿</span>
-	</a>
-</li>
+<a id="galleryAPPViewbtn" aria-label="相簿">
+    <svg width="20" height="20" viewBox="0 0 20 20" alt="">
+        <image x="0" y="0" width="20" height="20" preserveAspectRatio="xMinYMin meet" xlink:href="/apps/gallery/img/app.svg?v=d7726c1d" class="app-icon"></image>
+    </svg>
+    <span>相簿</span>
+</a>
 `
-document.querySelector('#appmenu').appendChild(div)
-document.querySelector('#galleryAPPViewbtn').addEventListener('click', openGalleryAPP)
+
+const mobile = 
+`
+<a id="galleryAPPViewbtn" aria-label="相簿">
+    <svg width="16" height="16" viewBox="0 0 16 16" alt="">
+        <defs><filter id="invertMenuMore-gallery"><feColorMatrix in="SourceGraphic" type="matrix" values="-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0"></feColorMatrix></filter></defs>
+        <image x="0" y="0" width="16" height="16" preserveAspectRatio="xMinYMin meet" filter="url(#invertMenuMore-gallery)" xlink:href="/apps/gallery/img/app.svg?v=9cc5153a" class="app-icon"></image>
+    </svg>
+    <span>相簿</span>
+</a>
+`
+
+document.querySelector('#appmenu li[data-id="gallery"]').innerHTML = desktop
+document.querySelector('#apps li[data-id="gallery"]').innerHTML = mobile
+document.querySelectorAll('#galleryAPPViewbtn').forEach(e => e.addEventListener('click', openGalleryAPP))
 
 function openGalleryAPP () {
 	let tg = document.querySelector('#galleryAPPViewbtn')
